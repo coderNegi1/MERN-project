a9993f7291d1a86ef8c0a2cd26b646c53659ea56
coderNegi1 <prashantnegi123321@gmail.com>
2025-03-28 15:37:08 +0530

    navbar ad routes update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/common/Navbar.jsx';
-import Login from './pages/login.jsx'
 
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-      <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
